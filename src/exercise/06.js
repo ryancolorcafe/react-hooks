877db2dac437f4f212c3373a6854925f076e844f
ryancolorcafe <ryan.brown@fastradius.com>
2022-05-24 09:45:25 -0700

    implement react-error-boundary package

--- src/exercise/06.js
+++ src/exercise/06.js
@@ -2,28 +2,13 @@
 // http://localhost:3000/isolated/exercise/06.js
 
 import * as React from 'react'
+import {ErrorBoundary} from 'react-error-boundary'
 import {
   fetchPokemon,
   PokemonInfoFallback,
   PokemonDataView,
   PokemonForm,
 } from '../pokemon'
-
-class ErrorBoundary extends React.Component {
-  state = {error: null}
-  static getDerivedStateFromError(error) {
-    return {error}
-  }
-
-  render() {
-    const {error} = this.state
-    if (error) {
-      return <this.props.FallbackComponent error={error} />
-    }
-
-    return this.props.children
-  }
-}
 
 function PokemonInfo({pokemonName}) {
   const [state, setState] = React.useState({
